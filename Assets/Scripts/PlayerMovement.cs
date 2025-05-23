7a513d52c236cdb839a791dd930ae94bbe5cd8d7
Noah Allen <NoahKRAllen@gmail.com>
2025-05-22 21:20:06 -0700

    Initial commit, hopefully with nothing unneccesary

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //Old Input System Setup
+    [SerializeField] private int movementSpeed = 5;
+    [SerializeField] private float rotationSpeed = 10f;
+    private void Update()
+    {
+        Vector2 inputVector = new Vector2(0,0);
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector.y += 1;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector.y -= 1;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector.x += 1;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector.x -= 1;
+        }
+        
+        inputVector = inputVector.normalized;
+        
+        Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
+        transform.position += moveDir * (Time.deltaTime * movementSpeed);
+        
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotationSpeed);
+    }
+}
