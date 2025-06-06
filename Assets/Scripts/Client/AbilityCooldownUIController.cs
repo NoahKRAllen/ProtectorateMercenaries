d6a11b7093ce95a3e3b7fc8b56c3233d6a242465
Noah Allen <NoahKRAllen@gmail.com>
2025-06-05 20:02:51 -0700

    Potential Camera and UI scripts, unsure yet

---
+++ Assets/Scripts/Client/AbilityCooldownUIController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class AbilityCooldownUIController : MonoBehaviour
+{
+    public static AbilityCooldownUIController Instance;
+    
+    [SerializeField] private Image aoeAbilityMask;
+    [SerializeField] private Image skillShotAbilityMask;
+    private void Awake()
+    {
+        if (Instance)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+    }
+    private void Start()
+    {
+        aoeAbilityMask.fillAmount = 0f;
+        skillShotAbilityMask.fillAmount = 0f;
+    }
+    public void UpdateAoeMask(float fillAmount)
+    {
+        aoeAbilityMask.fillAmount = fillAmount;
+    }
+    public void UpdateSkillShotMask(float fillAmount)
+    {
+        skillShotAbilityMask.fillAmount = fillAmount;
+    }
+}
--- Assets/Scripts/Client/SkillShotVisualDelay.cs
+++ Assets/Scripts/Client/SkillShotVisualDelay.cs
@@ -1,6 +1,25 @@
+using System.Collections;
 using UnityEngine;
 
+[RequireComponent(typeof(SpriteRenderer))]
 public class SkillShotVisualDelay : MonoBehaviour
-{
-    
+{ 
+    [SerializeField] private int delayFrameCount = 1;
+        
+    private SpriteRenderer _spriteRenderer;
+
+    private void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _spriteRenderer.enabled = false;
+    }
+
+    private IEnumerator Start()
+    {
+        for (var i = 0; i < delayFrameCount; i++)
+        {
+            yield return null;
+        }
+        _spriteRenderer.enabled = true;
+    }
 }
