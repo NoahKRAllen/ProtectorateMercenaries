f8df19b76e9fd315b8d7e8bd6e4d4c498d762f71
Noah Allen <NoahKRAllen@gmail.com>
2025-06-11 08:57:09 -0700

    Select to move is almost done

---
+++ Assets/Scripts/Client/InitializeMainCameraSystem.cs
@@ -0,0 +1,21 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+namespace Client
+{
+    [WorldSystemFilter(WorldSystemFilterFlags.ClientSimulation)]
+    public partial class InitializeMainCameraSystem : SystemBase
+    {
+        protected override void OnCreate()
+        {
+            RequireForUpdate<MainCamera>();
+        }
+
+        protected override void OnUpdate()
+        {
+            Enabled = false;
+            var mainCameraEntity = SystemAPI.GetSingletonEntity<MainCameraTag>();
+            EntityManager.SetComponentData(mainCameraEntity, new MainCamera{ Value = Camera.main });
+        }
+    }
+}
--- Assets/Scripts/Client/MercMoveInputSystem.cs
+++ Assets/Scripts/Client/MercMoveInputSystem.cs
@@ -1,6 +1,8 @@
-﻿using Unity.Entities;
+﻿using Common;
+using Unity.Entities;
 using Unity.NetCode;
 using Unity.Physics;
+using UnityEngine;
 using UnityEngine.InputSystem;
 
 namespace Client
@@ -9,8 +11,6 @@ namespace Client
     public partial class MercMoveInputSystem : SystemBase
     {
         private InputSystem_Actions _inputActions;
-        
-        //Won't need this as its for the click to move for later
         private CollisionFilter _selectionFilter;
         protected override void OnCreate()
         {
@@ -20,23 +20,55 @@ namespace Client
                 BelongsTo = 1 << 5, //Raycasts
                 CollidesWith = 1 << 0 //Ground Plane
             };
+            RequireForUpdate<OwnerMercTag>();
         }
-
+        
         protected override void OnStartRunning()
         {
             _inputActions.Enable();
             _inputActions.Player.Select.performed += OnSelectPositon;
+            _inputActions.Player.Move.performed += OnWASDInput;
         }
 
         protected override void OnStopRunning()
         {
             _inputActions.Disable();
-            _inputActions.Player.Move.performed -= OnSelectPositon;
+            _inputActions.Player.Select.performed -= OnSelectPositon;
+            _inputActions.Player.Move.performed -= OnWASDInput;
+            
         }
 
+        private void OnWASDInput(InputAction.CallbackContext context)
+        {
+            Debug.Log("Still need to make WASD Input controls");
+        }
+        
+        //TODO: Swap from select to move into WASD controlled twin-stick
+        //OnSelectPosition will not be the function for movement once tutorial is finished
+        //However, this function should work for left and right clicks instead
         private void OnSelectPositon(InputAction.CallbackContext context)
         {
-            
+            var collisionWorld = SystemAPI.GetSingleton<PhysicsWorldSingleton>().CollisionWorld;
+            var cameraEntity = SystemAPI.GetSingletonEntity<MainCameraTag>();
+            var mainCamera = EntityManager.GetComponentObject<MainCamera>(cameraEntity).Value;
+
+            var mousePosition = Input.mousePosition;
+            mousePosition.z = 100f;
+            var worldPosition = mainCamera.ScreenToWorldPoint(mousePosition);
+
+            var selectionInput = new RaycastInput
+            {
+                Start = mainCamera.transform.position,
+                End = worldPosition,
+                Filter = _selectionFilter
+            };
+
+            if (collisionWorld.CastRay(selectionInput, out var closestHit))
+            {
+                var mercEntity = SystemAPI.GetSingletonEntity<OwnerMercTag>();
+                //This line will be changed over to the select struct once WASD is implemented
+                EntityManager.SetComponentData(mercEntity, new MercMoveTargetPosition{Value = closestHit.Position});
+            }
         }
 
         protected override void OnUpdate()
