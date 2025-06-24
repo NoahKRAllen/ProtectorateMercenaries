bae3d4f98e6408623d51f2ca776f76d38a8554d0
Noah Allen <NoahKRAllen@gmail.com>
2025-06-24 13:03:18 -0700

    Progress made on DOTs based combat system

---
+++ Assets/Scripts/Common/CombatComponents.cs
@@ -0,0 +1,27 @@
+﻿using Unity.Entities;
+using Unity.NetCode;
+
+namespace Common
+{
+    public struct MaxHitPoints :IComponentData
+    {
+        public int Value;
+    }
+    public struct CurrentHitPoints :IComponentData
+    {
+        [GhostField] public int Value;
+    }
+    
+    [GhostComponent(PrefabType = GhostPrefabType.AllPredicted)]
+    public struct DamageBufferElement :IBufferElementData
+    {
+        public int Value;
+    }
+
+    [GhostComponent(PrefabType = GhostPrefabType.AllPredicted, OwnerSendType = SendToOwnerType.SendToNonOwner)]
+    public struct DamageThisTick : ICommandData
+    {
+        public NetworkTick Tick { get; set; }
+        public int Value;
+    }
+}
---
+++ Assets/Scripts/Common/HitPointsAuthoring.cs
@@ -0,0 +1,24 @@
+﻿using Unity.Entities;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Common
+{
+    public class HitPointsAuthoring : MonoBehaviour
+    {
+        public int maxHitPoints;
+
+        public class HitPointsBaker : Baker<HitPointsAuthoring>
+        {
+            public override void Bake(HitPointsAuthoring authoring)
+            {
+                var entity = GetEntity(TransformUsageFlags.Dynamic);
+                AddComponent(entity, new CurrentHitPoints {Value = authoring.maxHitPoints});
+                AddComponent(entity, new MaxHitPoints {Value = authoring.maxHitPoints});
+                AddBuffer<DamageBufferElement>(entity);
+                AddBuffer<DamageThisTick>(entity);
+
+            }
+        }
+    }
+}
