9e6c45792f83555e2969f55bf59ea7c8ff1feb86
Noah Allen <NoahKRAllen@gmail.com>
2025-06-24 22:48:08 -0700

    Good progress being made on an aoe ability

---
+++ Assets/Scripts/Common/AoeAspect.cs
@@ -0,0 +1,19 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+namespace Common
+{
+    public readonly partial struct AoeAspect : IAspect
+    {
+        private readonly RefRO<AbilityInput> _abilityInput;
+        private readonly RefRO<AbilityPrefabs> _abilityPrefabs;
+        private readonly RefRO<PlayerTeam> _mobaTeam;
+        private readonly RefRO<LocalTransform> _localTransform;
+
+        public bool ShouldAttack => _abilityInput.ValueRO.QAbility.IsSet;
+        public Entity AbilityPrefab => _abilityPrefabs.ValueRO.QAbility;
+        public PlayerTeam MobaTeam => _mobaTeam.ValueRO;
+        public float3 Position => _localTransform.ValueRO.Position;
+    }
+}
---
+++ Assets/Scripts/Common/BeginAoeAbilitySystem.cs
@@ -0,0 +1,21 @@
+﻿using Unity.Burst;
+using Unity.Entities;
+using Unity.NetCode;
+
+namespace Common
+{
+    [UpdateInGroup(typeof(PredictedSimulationSystemGroup))]
+    public partial struct BeginAoeAbilitySystem : ISystem
+    {
+        public void OnCreate(ref SystemState state)
+        {
+            state.RequireForUpdate<BeginSimulationEntityCommandBufferSystem.Singleton>();
+        }
+
+        [BurstCompile]
+        public void OnUpdate(ref SystemState state)
+        {
+            var ecbSingleton = SystemAPI.GetSingleton<BeginSimulationEntityCommandBufferSystem.Singleton>();
+        }
+    }
+}
