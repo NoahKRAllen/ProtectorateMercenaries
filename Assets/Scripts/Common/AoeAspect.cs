e2b98022e077e2a5843cbcade38a672a318339e8
Noah Allen <NoahKRAllen@gmail.com>
2025-06-27 12:06:43 -0700

    Tracking down interesting network bug

--- Assets/Scripts/Common/AoeAspect.cs
+++ Assets/Scripts/Common/AoeAspect.cs
@@ -8,12 +8,12 @@ namespace Common
     {
         private readonly RefRO<AbilityInput> _abilityInput;
         private readonly RefRO<AbilityPrefabs> _abilityPrefabs;
-        private readonly RefRO<PlayerTeam> _mobaTeam;
+        private readonly RefRO<PlayerTeam> _team;
         private readonly RefRO<LocalTransform> _localTransform;
 
         public bool ShouldAttack => _abilityInput.ValueRO.QAbility.IsSet;
         public Entity AbilityPrefab => _abilityPrefabs.ValueRO.QAbility;
-        public PlayerTeam MobaTeam => _mobaTeam.ValueRO;
-        public float3 Position => _localTransform.ValueRO.Position;
+        public PlayerTeam Team => _team.ValueRO;
+        public float3 AttackPosition => _localTransform.ValueRO.Position;
     }
 }
